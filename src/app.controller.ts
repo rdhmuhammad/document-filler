114e4be08bd1c4d1b4c47f1dd1a7f96419f38a17
rdhmuhammad <rdhmuhammad57@gmail.com>
2023-10-01 19:15:36 +0700

    feat: apply response meta to controller

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,7 +1,18 @@
-import {Body, Controller, Get, Post, UploadedFile, UseInterceptors, UsePipes, ValidationPipe} from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Post,
+  Res,
+  UploadedFile,
+  UseInterceptors,
+  UsePipes,
+  ValidationPipe
+} from '@nestjs/common';
 import {AppService} from './app.service';
 import {Response} from 'express';
 import {FileInterceptor} from "@nestjs/platform-express";
+import {ResponseMeta} from "./dto/response";
 
 @Controller()
 export class AppController {
@@ -9,8 +20,20 @@ export class AppController {
   @Post('/document')
   @UseInterceptors(FileInterceptor('pdf-file'))
   @UsePipes(new ValidationPipe({whitelist: true}))
-  async fillDocument(@UploadedFile() file : Express.Multer.File, @Body() args : Record<string, string>){
-   await this.appService.fillDocument(file.buffer, args)
+  async fillDocument(
+      @UploadedFile() file : Express.Multer.File,
+      @Body() args : Record<string, string>,
+      @Res() res: Response
+      ){
+   try {
+     await this.appService.fillDocument(file.buffer, args)
+     let body : ResponseMeta = {
+       message: "dokumen berhasil dibuat"
+     }
+     res.status(200).send()
+   }catch (e) {
+
+   }
   }
 
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -7,26 +7,28 @@ import * as Pizzip from 'pizzip';
 @Injectable()
 export class AppService {
 
-  async fillDocument(doc: Buffer, args: Record<string, string>){
-    const zip = new Pizzip(doc)
-    // @ts-ignore
+  async fillDocument(doc: Buffer, args: Record<string, string>) : Promise<any>{
+    return new Promise(()=>{
+      const zip = new Pizzip(doc)
+      // @ts-ignore
       const res = new Docxtemplater(zip,
-        {
-          paragraphLoop: true,
-          linebreaks: true,
-        })
+          {
+            paragraphLoop: true,
+            linebreaks: true,
+          })
 
-    res.render(args)
+      res.render(args)
 
-    const buf = res.getZip().generate({
-      type: "nodebuffer",
-      // compression: DEFLATE adds a compression step.
-      // For a 50MB output document, expect 500ms additional CPU time
-      compression: "DEFLATE",
-    });
+      const buf = res.getZip().generate({
+        type: "nodebuffer",
+        // compression: DEFLATE adds a compression step.
+        // For a 50MB output document, expect 500ms additional CPU time
+        compression: "DEFLATE",
+      });
 
 // buf is a nodejs Buffer, you can either write it to a
 // file or res.send it with express for example.
-    fs.writeFileSync(path.resolve('./', "output.docx"), buf);
+      fs.writeFileSync(path.resolve('./', "output.docx"), buf);
+    })
   }
 }
